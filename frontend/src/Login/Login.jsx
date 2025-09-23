29f91b458fb877863b2ff825ce7fcc65cd942b18
carlosMartinez404 <carlos.martinez.pro.2023@gmail.com>
2025-09-22 20:24:21 -0700

    front

---
+++ frontend/src/Login/Login.jsx
@@ -0,0 +1,20 @@
+import React, {useState} from "react";
+import styles from './Login.module.css';
+
+function Login(){
+    return(
+        <>
+            <div className={styles.root}>
+                <div className={styles.firstContainer}>
+                    
+                </div>
+
+                <div className={styles.secondContainer}>
+
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Login;
