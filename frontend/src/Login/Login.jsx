723e8d015d283e2c69f886316fddc4b7e3e0c984
carlosMartinez404 <carlos.martinez.pro.2023@gmail.com>
2025-09-27 13:56:56 -0700

    se trabajo la parte grafica del login

--- frontend/src/Login/Login.jsx
+++ frontend/src/Login/Login.jsx
@@ -3,24 +3,33 @@ import styles from './Login.module.css';
 
 function Login(){
     
-    useEffect(()=> {
-        fetch('http://localhost:8000/')
-        .then(response => response.json())
-        .then(data=> {
-            console.log(data)
-        })
-        .catch(error => console.log('Error:', error))
-    }, [])
+    // useEffect(()=> {
+    //     fetch('http://localhost:8000/')
+    //     .then(response => response.json())
+    //     .then(data=> {
+    //         console.log(data)
+    //     })
+    //     .catch(error => console.log('Error:', error))
+    // }, [])
 
     return(
         <>
-            <div className={styles.root}>
-                <div className={styles.firstContainer}>
-                    <h1>po</h1>
-                </div>
+            <div className={styles.mainView}>
+                <div className={styles.blueContainer}></div>
+                <div className={styles.loginContainer}>
+                    <form className={styles.loginForm}>
+                        <div className={styles.titleLoginTextContainer}>
+                            <h1 className={styles.titleLoginText}>Iniciar Sesión</h1>
+                        </div>
 
-                <div className={styles.secondContainer}>
-
+                        <div className={styles.inputLoginFormContainer}>
+                            <input type="text" className={styles.inputLoginForm} />
+                            <input type="password" className={`${styles.inputLoginForm} ${styles.separacion}`}/>
+                        </div>
+                        <div className={styles.submitButtonContainer}>
+                            <input type="submit" className={styles.submitButton} value="Iniciar Sesión"/>
+                        </div>
+                    </form>
                 </div>
             </div>
         </>
