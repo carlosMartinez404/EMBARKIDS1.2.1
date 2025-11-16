39ebd942221eb1ea08d2fd53dcfcc3f72a4e9678
carlosMartinez404 <carlos.martinez.pro.2023@gmail.com>
2025-11-15 18:36:36 -0700

    actualizacion

--- frontend/src/feactures/dashboard/components/Header.component.tsx
+++ frontend/src/feactures/dashboard/components/Header.component.tsx
@@ -1,8 +1,13 @@
 import styles from "./styles/Header.component.module.css"
+import Logo from "../../../../public/nodos.png"
+
 
 export function HeaderComponent() {
     return(
         <div className={styles.headerComponentMainContainer}>
+            <div className={styles.LogoIconContainer}>
+                <img className={styles.LogoIcon} src={Logo}/>
+            </div>
         </div>
     )
 }
--- frontend/src/feactures/dashboard/components/SideLeftMenu.component.tsx
+++ frontend/src/feactures/dashboard/components/SideLeftMenu.component.tsx
@@ -1,15 +1,23 @@
 import styles from "./styles/SideLeftMenu.component.module.css"
-
+import MenuIcon from "../../../assets/icons/MenuIcon.png"
+import AddProductIcon from "../../../assets/icons/AddProductIcon.png"
+import TruckIcon from "../../../assets/icons/TruckIcon.png"
 
 export function SideLeftMenu(){
     return(
         <div className={styles.SideLeftMenuMainContainer}>
             <div className={styles.OptionContainer}>
-                <label className={styles.Option}>Embarques</label>
+                <img className={`${styles.Icon} ${styles.MenuIcon}`} src={MenuIcon}/>
             </div>
 
             <div className={styles.OptionContainer}>
-                <label className={styles.Option}>Alta producto</label>
+                <img className={`${styles.Icon} ${styles.AddProductIcon}`} src={TruckIcon} />
+                <h1 className={styles.Option}>Embarques</h1>
+            </div>
+
+            <div className={styles.OptionContainer}>
+                <img className={`${styles.Icon} ${styles.AddProductIcon}`} src={AddProductIcon} />
+                <h1 className={styles.Option}>Alta producto</h1>
             </div>
         </div>
     )
